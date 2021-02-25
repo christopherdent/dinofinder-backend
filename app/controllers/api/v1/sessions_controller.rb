b1506ecdf29afa1911614a3be34d42f9c3c8f386
Christopher Dent <christopherdent01@gmail.com>
2021-02-24 22:04:11 -0500

    add sessions controller and routes

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+
+  end
+
+
+  def delete
+
+  end 
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,18 @@ namespace :api do
       end
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+        post 'login', to: 'sessions#create'
+        delete '/logout', to: 'sessions#destroy'
+      end
+    end 
+
+
 end
 
 
-##now we can only access dino_types through eras, and only access dinosaurs through dino_types 
+##now we can only access dino_types through eras, and only access dinosaurs through dino_types
 ##era/1/dino_types/2/dinosaur/1
 ##http:/localhost:3000/api/v1/eras/dino_typ
