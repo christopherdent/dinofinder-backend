6057799b822b2a653c3c3fe5f7a9c2017f2a902a
Christopher Dent <christopherdent01@gmail.com>
2021-02-26 23:35:46 -0500

    add sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,13 +1,26 @@
 class Api::V1::SessionsController < ApplicationController
 
-  def create
+    def create
 
+      # @user = User.find_by(email: params[:user][:email])
+      @user = User.find_by(email: params[:user][:email])
+      if @user && @user.authenticate(params[:user][:password])
+      # if @user && @user.authenticate(params[:user][:password])
+        render json: @user
+      else
+        render json: {
+          error: "Invalid credentials"
+        }, status: :unauthorized
+      end
+    end
+
+    def get_current_user
+      if logged_in?
+        render json: {
+            user: user_serializer(current_user)
+          }, status: :ok
+      else
+        render json: {error: "No current user"}
+      end
+    end
   end
-
-
-  def delete
-
-  end 
-
-
-end
