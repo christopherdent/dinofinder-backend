0260e07e086b7cc7c1c7eb2259dd0f917900f3a7
Christopher Dent <christopherdent01@gmail.com>
2021-02-27 21:45:58 -0500

    modify secrets controller

--- app/controllers/api/v1/secrets_controller.rb
+++ app/controllers/api/v1/secrets_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::SecretsController < ApplicationController
-  # before_action :set_secret, only: [:show, :update, :destroy]
+  before_action :set_secret, only: [:show, :update, :destroy]
 
   # GET /secrets
   def index
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,10 +2,9 @@ class Api::V1::SessionsController < ApplicationController
 
     def create
 
-      # @user = User.find_by(email: params[:user][:email])
       @user = User.find_by(email: params[:user][:email])
       if @user && @user.authenticate(params[:user][:password])
-      # if @user && @user.authenticate(params[:user][:password])
+
         render json: @user
       else
         render json: {
